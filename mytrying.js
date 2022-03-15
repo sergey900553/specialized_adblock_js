5b2b195fdc3fcefa6845562b683980ffef5433df
sergey900553 <42143402+sergey900553@users.noreply.github.com>
2022-03-15 19:15:59 +0300

    Add files via upload

---
+++ mytrying.js
@@ -0,0 +1,89 @@
+var container = document.querySelectorAll(`.videoBox.videoblock.js-pop.pcVideoListItem.canEdit`);
+var pornstar = document.querySelectorAll('.videoBox [href="/pornstar/robin-reid"]');
+var count = container.length;
+alert(count);
+
+
+
+
+
+
+var test = document.querySelectorAll('.videoBox a[href^="/pornstar/robin-reid"');
+
+
+document.querySelectorAll('.videoBox.videoblock.js-pop.pcVideoListItem.canEdit a[href^="/pornstar/robin-reid"]').forEach(e => e.remove());
+
+
+
+
+
+document.querySelectorAll('.videoBox.videoblock.js-pop.pcVideoListItem.canEdit a[href^="/pornstar/robin-reid"]').forEach(e => e.remove());
+
+
+document.querySelectorAll('li a[href^="/pornstar/robin-reid"').forEach(e => e.remove());
+
+
+
+
+document.querySelectorAll('li a[href^="/model/johnny-sins"').forEach(e => e.closest('li').remove());
+
+
+
+
+
+
+
+
+
+document.querySelectorAll('.videoBox.videoblock.js-pop.pcVideoListItem.canEdit a[href^="/model/johnny-sins"').forEach(e => e.closest('.videoBox.videoblock.js-pop.pcVideoListItem.canEdit').remove());
+
+
+
+
+
+
+
+
+
+
+
+let videoBox = document.querySelectorAll(`.videoBox.videoblock.js-pop.pcVideoListItem.canEdit`);
+let deletethis = document.querySelectorAll('[href="/pornstar/robin-reid"]');
+
+
+
+let bitch = document.querySelectorAll('.videoBox a[href^="/pornstar/robin-reid"]');
+if (bitch) {
+    document.querySelectorAll('.videoBox').forEach(e => e.remove());
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+const container = document.querySelectorAll('.videoBox a[href^="/pornstar/robin-reid"');
+removeAllChildNodes(container);
+
+
+
